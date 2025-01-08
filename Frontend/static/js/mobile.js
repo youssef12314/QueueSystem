2975cce041c452f762f71f1d6db8ad05182235f7
Youssef Talbi <112815657+youssef12314@users.noreply.github.com>
2025-01-08 12:19:38 +0100

    Update mobile.js

--- Frontend/static/js/mobile.js
+++ Frontend/static/js/mobile.js
@@ -4,24 +4,24 @@ async function fetchQueueStatus() {
 
         if (response.ok) {
             const data = await response.json();
-            console.log('Fetched data:', data); // Debug log
+            console.log('Fetched data:', data); 
 
             if (data.queue_number && data.dynamic_position !== undefined) {
-                // Update queue number and position
+                
                 document.getElementById('queue-number').textContent = data.queue_number;
                 document.getElementById('queue-position').textContent = data.dynamic_position;
 
-                // Redirect to "youre_next.html" if position is 1
+                
                 if (data.dynamic_position === 1) {
                     window.location.href = '/queue/youre_next';
-                    return; // Exit the function to avoid further updates
+                    return; 
                 }
 
-                // Update the estimated waiting time, default to "Calculating..." if not available
+                
                 let estimatedWaitingTime = data.estimated_waiting_time;
 
                 if (estimatedWaitingTime !== undefined) {
-                    // Round to the nearest whole number to remove decimal places
+                    
                     estimatedWaitingTime = Math.round(estimatedWaitingTime);
                 } else {
                     estimatedWaitingTime = "Calculating...";
@@ -31,9 +31,9 @@ async function fetchQueueStatus() {
             } else if (data.error) {
                 alert(data.error);
                 if (data.redirect) {
-                    window.location.href = data.redirect;  // Redirect to the new page
+                    window.location.href = data.redirect;  
                 } else {
-                    window.location.href = '/'; // Fallback, just in case
+                    window.location.href = '/'; 
                 }
             }
         } else {
@@ -46,8 +46,8 @@ async function fetchQueueStatus() {
     }
 }
 
-        // Poll the API every 1 second to check for updates
+        
         setInterval(fetchQueueStatus, 1000);
 
-        // Initial fetch on page load
+       
         fetchQueueStatus();
